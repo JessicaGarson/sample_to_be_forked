9069615ba2ebc456904ba2ec20f31fe9992bf525
jlg17nyu <36171782+jlg17nyu@users.noreply.github.com>
2018-02-11 22:32:09 -0500

    Create space.py

---
+++ space.py
@@ -0,0 +1,10 @@
+space = 19
+
+if space < 20:
+  print('We still have space left')
+elif space == 20:
+  print('We are just a capacity')
+elif space >= 15:
+  print('We are almost at capacity but we have a few spaces left')
+else:
+  print('Sorry, we are full')
